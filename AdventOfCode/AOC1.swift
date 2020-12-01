c47508364287043ab081a9f46d179302b34cdfa4
Dash Brittain <roxanne.brittain@gmail.com>
2020-11-30 21:13:43 -0800

    working code is better than good code, right?

--- AdventOfCode/AOC1.swift
+++ AdventOfCode/AOC1.swift
@@ -8,10 +8,26 @@ import Foundation
 
 struct AOC1: AOC {
     func solve1(input: String) -> Int {
-        input.integers.first ?? 0
+        for x in input.integers {
+            for y in input.integers {
+                if x + y == 2020 {
+                    return x * y
+                }
+            }
+        }
+        return 0
     }
     
     func solve2(input: String) -> Int {
-        input.integers.first ?? 0
+        for x in input.integers {
+            for y in input.integers {
+                for z in input.integers {
+                    if x + y + z == 2020 {
+                        return x * y * z
+                    }
+                }
+            }
+        }
+        return 0
     }
 }
