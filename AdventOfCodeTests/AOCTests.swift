1d5c8811f04c67106583692b03f0e389be408f1a
Dash Brittain <roxanne.brittain@gmail.com>
2020-12-01 22:26:11 -0800

    add tests

--- AdventOfCodeTests/AOCTests.swift
+++ AdventOfCodeTests/AOCTests.swift
@@ -18,5 +18,17 @@ class AOCTests: XCTestCase {
         XCTAssertEqual(AOC1().solve1(input: "1721,979,366,299,675,1456"), 514579)
         XCTAssertEqual(AOC1().solve2(input: "1721,979,366,299,675,1456"), 241861950)
     }
+    
+    func testAOC2() {
+        XCTAssertEqual(AOC2().solve1(input: "1-3 a: abcde"), 1)
+        XCTAssertEqual(AOC2().solve1(input: "1-3 b: cdefg"), 0)
+        XCTAssertEqual(AOC2().solve1(input: "2-9 c: ccccccccc"), 1)
+        XCTAssertEqual(AOC2().solve1(input: "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"), 2)
+        
+        XCTAssertEqual(AOC2().solve2(input: "1-3 a: abcde"), 1)
+        XCTAssertEqual(AOC2().solve2(input: "1-3 b: cdefg"), 0)
+        XCTAssertEqual(AOC2().solve2(input: "2-9 c: ccccccccc"), 0)
+        XCTAssertEqual(AOC2().solve2(input: "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"), 1)
+    }
 
 }
