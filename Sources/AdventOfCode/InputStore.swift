ae78e8a487b921a9726a1a8360d8ac7830fec0e4
Dash Brittain <roxanne.brittain@gmail.com>
2020-12-02 21:57:32 -0800

    create Input directory if needed

--- Sources/AdventOfCode/InputStore.swift
+++ Sources/AdventOfCode/InputStore.swift
@@ -25,25 +25,26 @@ struct InputStore {
     }
     
     func input(for puzzleIndex: Int, forceDownload: Bool) -> String {
-        let filename = "Input/\(puzzleIndex).txt"
-        if !forceDownload, let input = loadInput(fromFileNamed: filename) {
+        let directory = "Input"
+        let filename = "\(puzzleIndex).txt"
+        if !forceDownload, let input = loadInput(fromFileNamed: filename, inDirectory: directory) {
             print("Using cached input")
             return input
         } else {
-            return downloadInput(for: puzzleIndex, toFileNamed: filename)
+            return downloadInput(for: puzzleIndex, toFileNamed: filename, inDirectory: directory)
         }
     }
     
-    private func loadInput(fromFileNamed filename: String) -> String? {
-        let fileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(filename)
+    private func loadInput(fromFileNamed filename: String, inDirectory directory: String) -> String? {
+        let directoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(directory)
         do {
-            return try String(contentsOf: fileURL)
+            return try String(contentsOf: directoryURL.appendingPathComponent(filename))
         } catch {
             return nil
         }
     }
     
-    private func downloadInput(for puzzleIndex: Int, toFileNamed filename: String) -> String {
+    private func downloadInput(for puzzleIndex: Int, toFileNamed filename: String, inDirectory directory: String) -> String {
         let inputURLString = "https://adventofcode.com/2020/day/\(puzzleIndex)/input"
         print("Downloading input from \(inputURLString)")
         
@@ -60,7 +61,14 @@ struct InputStore {
         
         print("Downloaded \(input.lines.count) lines (\(input.count) chars) of input")
         
-        let url = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(filename)
+        let directoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath).appendingPathComponent(directory)
+        do {
+            try FileManager().createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
+        } catch {
+            fatalError("Could not create directory (\(directory))")
+        }
+        
+        let url = directoryURL.appendingPathComponent(filename)
         do {
             try input.write(to: url, atomically: true, encoding: .utf8)
         } catch {
