912a3aef49e1ddbeec8026a825fdb82a1b3adeed
Dash Brittain <roxanne.brittain@gmail.com>
2020-12-09 22:16:41 -0800

    better code

--- Sources/AdventOfCode/Puzzles/AOC10.swift
+++ Sources/AdventOfCode/Puzzles/AOC10.swift
@@ -6,55 +6,40 @@
 
 import Foundation
 
-
-var map: [Int : Int] = [:]
-
 struct AOC10: Puzzle {
-    func solve1(input: String) -> Int {
-        var ints = input.integers
-        ints.append(ints.max()! + 3)
-        ints.sort()
-        let set = NSCountedSet()
-        var last = 0
-        for i in ints {
-            set.add(i - last)
-            last = i
-        }
-        return set.count(for: 1) * set.count(for: 3)
+    func parseAdapters(from input: String) -> [Int] {
+        let adaptersInBag = input.integers.sorted()
+        guard let maxInBag = adaptersInBag.last else { fatalError() }
+        return [0] + adaptersInBag + [maxInBag + 3]
     }
     
-    func solCount(ints: [Int]) -> Int {
-        if ints.count < 2 {
+    func countArrangements(ofSortedAdapters adapters: [Int], maxJoltDiff: Int, cachedCountsByAdapter: inout [Int: Int]) -> Int {
+        guard let first = adapters.first, adapters.count > 1 else {
             return 1
         }
-        if let i = ints.first, let m = map[i] {
-            return m
+        if let cachedTotal = cachedCountsByAdapter[first] {
+            return cachedTotal
         }
         var total = 0
-        if ints[1] - ints[0] <= 3 {
-            total += solCount(ints: Array(ints[1...]))
-        }
-        if ints.count >= 3 {
-            if ints[2] - ints[0] <= 3 {
-                total += solCount(ints: Array(ints[2...]))
+        for diff in 1...maxJoltDiff {
+            if let next = adapters[safe: diff], next - first <= maxJoltDiff {
+                total += countArrangements(ofSortedAdapters: Array(adapters[diff...]), maxJoltDiff: maxJoltDiff, cachedCountsByAdapter: &cachedCountsByAdapter)
             }
         }
-        if ints.count >= 4 {
-            if ints[3] - ints[0] <= 3 {
-                total += solCount(ints: Array(ints[3...]))
-            }
-        }
-        map[ints.first!] = total
+        cachedCountsByAdapter[first] = total
         return total
     }
     
+    func solve1(input: String) -> Int {
+        let adapters = parseAdapters(from: input)
+        let joltDiffs = zip(adapters.dropFirst(), adapters).map(-)
+        let set = joltDiffs.reduce(into: NSCountedSet(), { $0.add($1) })
+        return set.count(for: 1) * set.count(for: 3)
+    }
+    
     func solve2(input: String) -> Int {
-        var ints = input.integers
-        ints.append(ints.max()! + 3)
-        ints.append(0)
-        ints.sort()
-        print(ints)
-        
-        return solCount(ints: ints)
+        let adapters = parseAdapters(from: input)
+        var cachedCountsByAdapter: [Int : Int] = [:]
+        return countArrangements(ofSortedAdapters: adapters, maxJoltDiff: 3, cachedCountsByAdapter: &cachedCountsByAdapter)
     }
 }
--- Sources/AdventOfCode/Utilities/Sequence.swift
+++ Sources/AdventOfCode/Utilities/Sequence.swift
@@ -26,3 +26,13 @@ extension Sequence {
         })
     }
 }
+
+extension Array {
+    public subscript(safe index: Int) -> Element? {
+        guard index >= 0, index < endIndex else {
+            return nil
+        }
+
+        return self[index]
+    }
+}
